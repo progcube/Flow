f144736a20e00e8c91e6af1db7a3eaba4c0f519a
Maxime Leboeuf <ml@maximeleboeuf.com>
2019-02-10 22:54:12 -0500

    Cleanup

--- Progcube.Flow.NodeLibraryImpl/Npgsql/NpgsqlCommandNodeImpl.cs
+++ Progcube.Flow.NodeLibraryImpl/Npgsql/NpgsqlCommandNodeImpl.cs
@@ -1,18 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace Progcube.Flow.NodeLibraryImpl.Npgsql
 {
     public class NpgsqlCommandNodeImpl : NodeImplementation<NodeLibrary.Npgsql.NpgsqlCommandNode>
     {
-        public List<string> Errors => new List<string>();
-
-        public NpgsqlCommandNodeImpl()
-        {
-            //Out = new ComputedValue<string>();
-        }
-
         public override void Evaluate()
         {
             throw new NotImplementedException();
